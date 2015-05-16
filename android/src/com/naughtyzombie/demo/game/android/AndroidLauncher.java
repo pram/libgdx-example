c39821efd7f3c86494812cb7db3c111d18fa11bf
pram <pram@attale.com>
2015-05-16 11:23:53 +0100

    adding basic installation

---
+++ android/src/com/naughtyzombie/demo/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.naughtyzombie.demo.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.naughtyzombie.demo.game.MyDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyDemo(), config);
+	}
+}
