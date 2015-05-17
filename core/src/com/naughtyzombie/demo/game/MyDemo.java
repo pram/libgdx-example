b178c62a4e38f6cd2af39c123b315db6dcd41500
pram <pram@attale.com>
2015-05-17 16:38:16 +0100

    adding constants file

--- core/src/com/naughtyzombie/demo/game/MyDemo.java
+++ core/src/com/naughtyzombie/demo/game/MyDemo.java
@@ -20,6 +20,7 @@ public class MyDemo extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
---
+++ core/src/com/naughtyzombie/demo/game/util/Constants.java
@@ -0,0 +1,12 @@
+package com.naughtyzombie.demo.game.util;
+
+/**
+ * Created by pram on 17/05/2015.
+ */
+public class Constants {
+    //Set the visible game world width in meters
+    public static final float VIEWPORT_WIDTH = 5.0f;
+
+    //set the visible game world height in meters
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+}
