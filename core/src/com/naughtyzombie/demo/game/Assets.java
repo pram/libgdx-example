65eff2db66eb792c7309eb566508a8ab0c6fac44
pram <pram@attale.com>
2015-05-18 12:04:24 +0100

    adding gui

--- core/src/com/naughtyzombie/demo/game/Assets.java
+++ core/src/com/naughtyzombie/demo/game/Assets.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetErrorListener;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
 
@@ -21,6 +22,8 @@ public class Assets implements Disposable, AssetErrorListener {
     public static final String TAG = Assets.class.getName();
     public static final Assets instance = new Assets();
     private AssetManager assetManager;
+
+    public AssetFonts fonts;
 
     public AssetBunny bunny;
     public AssetRock rock;
@@ -53,6 +56,7 @@ public class Assets implements Disposable, AssetErrorListener {
         }
 
         // create game resource objects
+        fonts = new AssetFonts();
         bunny = new AssetBunny(atlas);
         rock = new AssetRock(atlas);
         goldCoin = new AssetGoldCoin(atlas);
@@ -61,18 +65,22 @@ public class Assets implements Disposable, AssetErrorListener {
     }
 
     @Override
-    public void dispose () {
+    public void dispose() {
         assetManager.dispose();
+        fonts.defaultSmall.dispose();
+        fonts.defaultNormal.dispose();
+        fonts.defaultBig.dispose();
     }
 
     @Override
     public void error(AssetDescriptor asset, Throwable throwable) {
-        Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", (Exception)throwable);
+        Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", (Exception) throwable);
     }
 
     public class AssetBunny {
         public final AtlasRegion head;
-        public AssetBunny (TextureAtlas atlas) {
+
+        public AssetBunny(TextureAtlas atlas) {
             head = atlas.findRegion("bunny_head");
         }
     }
@@ -89,14 +97,16 @@ public class Assets implements Disposable, AssetErrorListener {
 
     public class AssetGoldCoin {
         public final AtlasRegion goldCoin;
-        public AssetGoldCoin (TextureAtlas atlas) {
+
+        public AssetGoldCoin(TextureAtlas atlas) {
             goldCoin = atlas.findRegion("item_gold_coin");
         }
     }
 
     public class AssetFeather {
         public final AtlasRegion feather;
-        public AssetFeather (TextureAtlas atlas) {
+
+        public AssetFeather(TextureAtlas atlas) {
             feather = atlas.findRegion("item_feather");
         }
     }
@@ -108,13 +118,41 @@ public class Assets implements Disposable, AssetErrorListener {
         public final AtlasRegion mountainLeft;
         public final AtlasRegion mountainRight;
         public final AtlasRegion waterOverlay;
-        public AssetLevelDecoration (TextureAtlas atlas) {
+
+        public AssetLevelDecoration(TextureAtlas atlas) {
             cloud01 = atlas.findRegion("cloud01");
             cloud02 = atlas.findRegion("cloud02");
             cloud03 = atlas.findRegion("cloud03");
             mountainLeft = atlas.findRegion("mountain_left");
             mountainRight = atlas.findRegion("mountain_right");
             waterOverlay = atlas.findRegion("water_overlay");
+        }
+    }
+
+    public class AssetFonts {
+        public final BitmapFont defaultSmall;
+        public final BitmapFont defaultNormal;
+        public final BitmapFont defaultBig;
+
+        public AssetFonts() {
+            // create three fonts using Libgdx's 15px bitmap font
+            defaultSmall = new BitmapFont(
+                    Gdx.files.internal("images/arial-15.fnt"), true);
+            defaultNormal = new BitmapFont(
+                    Gdx.files.internal("images/arial-15.fnt"), true);
+            defaultBig = new BitmapFont(
+                    Gdx.files.internal("images/arial-15.fnt"), true);
+            // set font sizes
+            defaultSmall.getData().setScale(0.75f);
+            defaultNormal.getData().setScale(1.0f);
+            defaultBig.getData().setScale(2.0f);
+            // enable linear texture filtering for smooth fonts
+            defaultSmall.getRegion().getTexture().setFilter(
+                    TextureFilter.Linear, TextureFilter.Linear);
+            defaultNormal.getRegion().getTexture().setFilter(
+                    TextureFilter.Linear, TextureFilter.Linear);
+            defaultBig.getRegion().getTexture().setFilter(
+                    TextureFilter.Linear, TextureFilter.Linear);
         }
     }
 
