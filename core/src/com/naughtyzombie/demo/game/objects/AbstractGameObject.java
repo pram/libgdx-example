d3d581530b697862411842a3ce5cdc3e81d37a71
pram <pram@attale.com>
2015-05-18 12:24:50 +0100

    adding movement variables

--- core/src/com/naughtyzombie/demo/game/objects/AbstractGameObject.java
+++ core/src/com/naughtyzombie/demo/game/objects/AbstractGameObject.java
@@ -5,6 +5,7 @@ package com.naughtyzombie.demo.game.objects;
  */
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class AbstractGameObject {
@@ -14,12 +15,23 @@ public abstract class AbstractGameObject {
     public Vector2 scale;
     public float rotation;
 
+    public Vector2 velocity;
+    public Vector2 terminalVelocity;
+    public Vector2 friction;
+    public Vector2 acceleration;
+    public Rectangle bounds;
+
     public AbstractGameObject() {
         position = new Vector2();
         dimension = new Vector2(1, 1);
         origin = new Vector2();
         scale = new Vector2(1, 1);
         rotation = 0;
+        velocity = new Vector2();
+        terminalVelocity = new Vector2(1, 1);
+        friction = new Vector2();
+        acceleration = new Vector2();
+        bounds = new Rectangle();
     }
 
     public void update(float deltaTime) {
