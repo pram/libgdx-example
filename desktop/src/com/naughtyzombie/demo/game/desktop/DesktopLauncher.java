503fad17cb9c1cd5e00d8d0aa847c0a534982f0d
pram <pram@attale.com>
2015-05-18 09:47:17 +0100

    adding assets

--- desktop/src/com/naughtyzombie/demo/game/desktop/DesktopLauncher.java
+++ desktop/src/com/naughtyzombie/demo/game/desktop/DesktopLauncher.java
@@ -2,10 +2,27 @@ package com.naughtyzombie.demo.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.naughtyzombie.demo.game.MyDemo;
 
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+
 public class DesktopLauncher {
+
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+
 	public static void main (String[] arg) {
+        if (rebuildAtlas) {
+            Settings settings = new Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.duplicatePadding = false;
+            settings.debug = drawDebugOutline;
+            TexturePacker.process(settings, "assets-raw/images", "../android/assets/images", "jumpyjump.pack");
+        }
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyDemo(), config);
 	}
