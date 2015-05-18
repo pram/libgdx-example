e01d19bbc91f515321866e9b3c8b99dea60eb342
pram <pram@attale.com>
2015-05-18 10:23:46 +0100

    adding level assets

--- core/src/com/naughtyzombie/demo/game/Assets.java
+++ core/src/com/naughtyzombie/demo/game/Assets.java
@@ -79,4 +79,21 @@ public class Assets implements Disposable, AssetErrorListener {
         }
     }
 
+    public class AssetLevelDecoration {
+        public final AtlasRegion cloud01;
+        public final AtlasRegion cloud02;
+        public final AtlasRegion cloud03;
+        public final AtlasRegion mountainLeft;
+        public final AtlasRegion mountainRight;
+        public final AtlasRegion waterOverlay;
+        public AssetLevelDecoration (TextureAtlas atlas) {
+            cloud01 = atlas.findRegion("cloud01");
+            cloud02 = atlas.findRegion("cloud02");
+            cloud03 = atlas.findRegion("cloud03");
+            mountainLeft = atlas.findRegion("mountain_left");
+            mountainRight = atlas.findRegion("mountain_right");
+            waterOverlay = atlas.findRegion("water_overlay");
+        }
+    }
+
 }
