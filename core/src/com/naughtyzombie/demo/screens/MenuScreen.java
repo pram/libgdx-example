dc4204389b138ff8d4c40798fa8d6bfcd3dde5d1
pram <pram@attale.com>
2015-05-20 07:27:32 +0100

    adding animation to menu screen

--- core/src/com/naughtyzombie/demo/screens/MenuScreen.java
+++ core/src/com/naughtyzombie/demo/screens/MenuScreen.java
@@ -1,11 +1,14 @@
 package com.naughtyzombie.demo.screens;
 
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
@@ -282,11 +285,27 @@ public class MenuScreen extends AbstractGameScreen {
         // + Coins
         imgCoins = new Image(skinJumpyJump, "coins");
         layer.addActor(imgCoins);
-        imgCoins.setPosition(135, 80);
+        imgCoins.setOrigin(imgCoins.getWidth() / 2,
+                imgCoins.getHeight() / 2);
+        imgCoins.addAction(sequence(
+                moveTo(135, -20),
+                scaleTo(0, 0),
+                fadeOut(0),
+                delay(2.5f),
+                parallel(moveBy(0, 100, 0.5f, Interpolation.swingOut),
+                        scaleTo(1.0f, 1.0f, 0.25f, Interpolation.linear),
+                        alpha(1.0f, 0.5f))));
+        /*imgCoins.setPosition(135, 80);*/
         // + Bunny
         imgBunny = new Image(skinJumpyJump, "bunny");
         layer.addActor(imgBunny);
-        imgBunny.setPosition(355, 40);
+        imgBunny.addAction(sequence(
+                moveTo(655, 510),
+                delay(4.0f),
+                moveBy(-70, -100, 0.5f, Interpolation.fade),
+                moveBy(-100, -50, 0.5f, Interpolation.fade),
+                moveBy(-150, -300, 1.0f, Interpolation.elasticIn)));
+        /*imgBunny.setPosition(355, 40);*/
         return layer;
     }
 
